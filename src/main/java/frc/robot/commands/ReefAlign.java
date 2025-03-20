9dcf6017fcf57f7b047c0add8ec9492fdc841a10
BananasAmIRite <jasony1230@gmail.com>
2025-03-19 20:14:09 -0400

    fix: reverted reef align distances back to hatboro

--- src/main/java/frc/robot/commands/ReefAlign.java
+++ src/main/java/frc/robot/commands/ReefAlign.java
@@ -41,9 +41,9 @@ public class ReefAlign {
   public static final Map<Integer, Pose2d> centerAlignPoses = new HashMap<>();
   public static final Map<Integer, Pose2d> rightAlignPoses = new HashMap<>();
 
-  private static final Distance kLeftAlignDistance = Inches.of(-7.6);
+  private static final Distance kLeftAlignDistance = Inches.of(-9.5);
   private static final Distance kReefDistance = Inches.of(17.5);
-  private static final Distance kRightAlignDistance = Inches.of(4.8);
+  private static final Distance kRightAlignDistance = Inches.of(3.4);
   private static final Distance kIntermediateDistance = Inches.of(-10);
 
   private static final Rotation2d kReefAlignmentRotation = Rotation2d.k180deg;
